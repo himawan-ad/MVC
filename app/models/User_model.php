dcffa9741ffcaae879ad05728b0859d4ee879ce6
Himawan Addillah <68602920+himawan-ad@users.noreply.github.com>
2021-01-16 19:22:31 +0700

    Add files via upload

---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model {
+    private $nama = 'Himawan';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ app/views/about/index.php
@@ -0,0 +1,6 @@
+
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Himawan Addillah" width="200" height="175" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya seorang <?= $data['pekerjaan']; ?></p>
+</div>
